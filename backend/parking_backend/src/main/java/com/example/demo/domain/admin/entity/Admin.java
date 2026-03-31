71d77aa6d57753e496e4c7be12c938bc7ed972f6
BoKyungKim <qhzud1207@gmail.com>
2026-03-31 11:39:21 +0900

    adminAuth

---
+++ backend/parking_backend/src/main/java/com/example/demo/domain/admin/entity/Admin.java
@@ -0,0 +1,40 @@
+package com.example.demo.domain.admin.entity;
+
+import com.example.demo.domain.admin.enums.AdminStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "Admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+    @Comment("관리자 로그인 ID")
+    @Column(unique = true,nullable = false,length = 100)
+    private String loginId;
+    @Comment("암호화된 비밀번호")
+    @Column(nullable = false,length = 255)
+    private String password;
+    @Comment("관리자 이름")
+    @Column(nullable = false,length = 100)
+    private String name;
+    @Enumerated(EnumType.STRING)
+    @Comment("계정상태")
+    @Column(nullable = false)
+    private AdminStatus status = AdminStatus.ACTIVE;
+    @CreationTimestamp // 생성 시간 자동 입력
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLoginAt;
+    private LocalDateTime deletedAt;
+}
---
+++ backend/parking_backend/src/test/java/com/example/demo/PasswordTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTest {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+}
