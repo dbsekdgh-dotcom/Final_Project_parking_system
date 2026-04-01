f220df7f89e3180edaffcc1439f68e56bda7a038
ehrbs56 <ehrbs56@gmail.com>
2026-04-01 15:12:44 +0900

    docs: Household 엔티티 수정

--- backend/parking_backend/src/main/java/com/example/demo/domain/shared/household/Household.java
+++ backend/parking_backend/src/main/java/com/example/demo/domain/shared/household/Household.java
@@ -1,4 +1,17 @@
 package com.example.demo.domain.shared.household;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Household {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long householdId;
+    private Integer unitNo;
 }
