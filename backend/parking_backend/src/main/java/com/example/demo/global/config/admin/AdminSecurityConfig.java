d66983ed1a4d10761d47db4831aef1100889cac5
juyeon <chlwndus120@gmail.com>
2026-04-02 20:54:51 +0900

    테스트를 위해 설정했던 시큐리티 임시 해제 코드 삭제

--- backend/parking_backend/src/main/java/com/example/demo/global/config/admin/AdminSecurityConfig.java
+++ backend/parking_backend/src/main/java/com/example/demo/global/config/admin/AdminSecurityConfig.java
@@ -55,7 +55,6 @@ public class AdminSecurityConfig {
         http.authorizeHttpRequests(auth->auth
                 .requestMatchers("/admin/login","/admin/refresh").permitAll() // 로그인 경로는 누구나 접근 가능
                 .requestMatchers("/admin/**").hasRole("ADMIN") // 나머지 관리자 APT는 권한 필요
-                .requestMatchers("/api/prepays/**").permitAll() // 테스트를 위해 잠깐 넣어둡니다....
                 .anyRequest().authenticated()
         );
 
@@ -72,7 +71,7 @@ public class AdminSecurityConfig {
         // 예외 처리 (권한 부족 시) -- 추후 확장성 고려해 작성함
         http.exceptionHandling(ex->ex.accessDeniedHandler(adminAccessDeniedHandler));
         // JWT 필터 추가
-        //http.addFilterBefore(new JwtAuthenticationFilter(adminJWTUtil), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(adminJWTUtil), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
