d7a38df82fa44ad24fd9add9e2e64e9672405daa
greathera <leejy3658@gmail.com>
2026-04-02 20:13:23 +0900

    회원정보 차량정보 등록 작성중

--- frontend/parking_frontend_user/src/App.jsx
+++ frontend/parking_frontend_user/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainLayout from './shared/layouts/MainLayout.jsx'
-import DashBoard from './features/dash/DashBoard.jsx'
+import DashBoard from './features/dash/pages/DashBoard.jsx'
 import LoginPage from './features/auth/pages/LoginPage.jsx' // 1. 로그인 페이지 가져오기
 import SignupPage from './features/auth/pages/SignupPage.jsx'
 import OAuthRedirectPage from './features/auth/pages/OAuthRedirectPage.jsx'
