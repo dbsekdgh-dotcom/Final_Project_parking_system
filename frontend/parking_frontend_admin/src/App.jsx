2b713a6bd4d44891c8973f5f8998ba3fe32f5c37
BoKyungKim <qhzud1207@gmail.com>
2026-04-02 15:41:36 +0900

    admin 프론트

--- frontend/parking_frontend_admin/src/App.jsx
+++ frontend/parking_frontend_admin/src/App.jsx
@@ -1,20 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Mainlayout from "./shared/layouts/Mainlayout"
 import DashBoard from "./features/dashboard/pages/DashBoard"
 import ParkingSpace from "./features/parkingspace/pages/ParkingSpace"
+import LoginPage from "./features/auth/pages/LoginPage"
 
 
 function App() {
- 
+
 
   return (
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        {/* <Route path="/admin" element={<LoginPage />}/> */}
-    
+        {/* 1. 레이아웃이 없는 독립 페이지(로그인) */}
+        <Route path="/admin" element={<LoginPage />}/>
+
+        {/* 2. 공통 레이아웃(헤어, 사이드바)이 적용되는 관리자 페이지들 */}
         <Route element={<Mainlayout />}>
           <Route path="/admin/dashboard" element={<DashBoard />} />
         </Route>
+
+        {/* 예외 처리: 아무것도 없는"/"로 접속하거나 잘못된 경로일 때 */}
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+        <Route path="*" element={<Navigate to="/admin" replace />} />
       </Routes>
     </BrowserRouter>
   )
