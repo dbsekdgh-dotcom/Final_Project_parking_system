29d5c0b357824cb8b5af783c25702dacd27ee36c
dbsekdgh <dbsekdgh09@gmail.com>
2026-04-03 14:28:07 +0900

    s3update

--- ai-service/app/domain/entry/entryimagesave/entryimagesaveservice.py
+++ ai-service/app/domain/entry/entryimagesave/entryimagesaveservice.py
@@ -1,16 +1,21 @@
 import boto3
 import uuid
 from datetime import datetime
+import os
 
 s3_client=boto3.client(
     "s3",
-    aws_access_key_id="AWS_ACCESS_KEY_ID",
-    aws_secret_access_key="AWS_SECRET_ACCESS_KEY",
-    region_name="S3_REGION"
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("S3_REGION")
 )
-BUCKET_NAME="S3_BUCKET_NAME"
+BUCKET_NAME=os.getenv("S3_BUCKET_NAME")
 
 def upload_file_to_s3(file)-> str:
     filename =f"{datetime.now().strftime('%Y%m%d_%H%M%s')}_{uuid.uuid4().hex}.jpg"
-    s3_client.upload_fileobj(file.file, BUCKET_NAME, filename)
+    s3_client.upload_fileobj(file.file,
+                             BUCKET_NAME,
+                             filename,
+                             ExtraArgs={"ContentType": "image/jpeg"}
+                             )
     return f"https://{BUCKET_NAME}.s3.{s3_client.meta.region_name}.amazonaws.com/{filename}"
--- ai-service/main.py
+++ ai-service/main.py
@@ -1,11 +1,13 @@
 import numpy as np
 import cv2
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.domain.entryexitocr.router import entryexit_router
-
+from app.domain.entry.entryimagesave.entryimagesaveRouter import s3_router
 app = FastAPI()
 
+print("AWS_REGION =", os.getenv("AWS_REGION"))
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
@@ -15,7 +17,16 @@ app.add_middleware(
 )
 
 app.include_router(
+    
     entryexit_router,
     prefix="/api/v1/parking/entryexit",
     tags=["EntryExit OCR"]
+    
 )
+
+app.include_router(
+    s3_router,
+    prefix="/api/v1/s3",
+    tags=["S3 Upload"]
+)
+
