97300b9a40dab7e555e0cd8f1aef562218fa43cd
ehrbs56 <ehrbs56@gmail.com>
2026-04-09 12:17:13 +0900

    사용자 로그인 최종

--- backend/parking_backend/src/main/java/com/example/demo/domain/user/auth/repository/SocialAccountRepository.java
+++ backend/parking_backend/src/main/java/com/example/demo/domain/user/auth/repository/SocialAccountRepository.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.user.entity.SocialAccount;
 import com.example.demo.domain.user.enums.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
