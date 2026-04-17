08e4842287332ecabf6e300fc49348ef0700aaf4
greathera <leejy3658@gmail.com>
2026-04-17 13:15:57 +0900

    feat:홈화면 프론트

--- frontend/parking_frontend_user/src/features/dash/pages/DashBoard.jsx
+++ frontend/parking_frontend_user/src/features/dash/pages/DashBoard.jsx
@@ -32,7 +32,7 @@ const DashBoard = () => {
     //백엔드 데이터 호출
     const fetchDashboard = async () =>{
       try {
-        const response = await axios.get("http://localhost:8081/api/dashboard?userId=1");
+        const response = await axios.get("http://localhost:8081/api/dashboard?userId=69");
         setData(response.data);
       }catch (error){
         console.error("데이터 로드 실패:", error);
@@ -65,7 +65,7 @@ const DashBoard = () => {
               <div className="floor-name">{floor.floorName}</div>
               <div className="floor-type">{floor.description}</div>
               <div className="floor-availability">
-                <span className="available">{floor.available}</span> / {floor.total} 가용
+                <span className="available">{floor.available}</span> / {floor.total} 
               </div>
               <div className="progress-bar">
                 <div
