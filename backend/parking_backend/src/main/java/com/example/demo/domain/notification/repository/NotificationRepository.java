1a4617eb92fbf7f6cfdcdc924764bcc0acd3e879
ehrbs56 <ehrbs56@gmail.com>
2026-04-22 13:05:46 +0900

    feat: 회원 탈퇴 예외 처리 및 신청 기능 상태값 변화

--- backend/parking_backend/src/main/java/com/example/demo/domain/notification/repository/NotificationRepository.java
+++ backend/parking_backend/src/main/java/com/example/demo/domain/notification/repository/NotificationRepository.java
@@ -2,7 +2,15 @@ package com.example.demo.domain.notification.repository;
 
 import com.example.demo.domain.notification.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
 
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Notification n SET n.deletedAt = :now WHERE n.user.userId = :userId AND n.deletedAt IS NULL")
+    void softDeleteByUserId(@Param("userId") Long userId, @Param("now") LocalDateTime now);
 }
