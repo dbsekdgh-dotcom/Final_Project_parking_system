542806027312bf9a72e17127346a8ed526cd36da
juyeon <chlwndus120@gmail.com>
2026-04-24 10:51:15 +0900

    front test

--- frontend/parking_frontend_kiosk/src/main.jsx
+++ frontend/parking_frontend_kiosk/src/main.jsx
@@ -8,7 +8,7 @@ const queryClient=new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/koisk">
+    <BrowserRouter basename="/kiosk">
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
