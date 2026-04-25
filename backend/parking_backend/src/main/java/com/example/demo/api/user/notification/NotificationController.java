9bf8299d783e8a46a1cab8b27cdef1d1eaf4783d
greathera <leejy3658@gmail.com>
2026-04-25 17:09:47 +0900

    feat: 알림 삭제 기능 구현 및 로직 수정 중(401에러 디버깅중)

--- backend/parking_backend/src/main/java/com/example/demo/api/user/notification/NotificationController.java
+++ backend/parking_backend/src/main/java/com/example/demo/api/user/notification/NotificationController.java
@@ -5,13 +5,15 @@ import com.example.demo.domain.auth.user.principal.PrincipalDetails;
 import com.example.demo.domain.notification.dtos.NotificationResponseDto;
 import com.example.demo.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
@@ -24,10 +26,26 @@ public class NotificationController {
     public ResponseEntity<List<NotificationResponseDto>> getNotifications(
             @AuthenticationPrincipal PrincipalDetails principalDetails){
 
+//        Long userId = principalDetails.getUser().getUserId();
+//
+//        List<NotificationResponseDto> notifications = notificationService.getNotifications(userId);
+//
+//        return ResponseEntity.ok(notifications);
+//       보안체크
+        if (principalDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        // 로그인된 유저의 실제 Id 추출
         Long userId = principalDetails.getUser().getUserId();
+
+        System.out.println("로그인 정보가 없어 임시로 1번 유저 데이터를 사용합니다.");
+
+        // 해당 유저의 알림 목록 조회
         List<NotificationResponseDto> notifications = notificationService.getNotifications(userId);
 
         return ResponseEntity.ok(notifications);
+
 
     }
 
