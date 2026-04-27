9af8ac09c4206805c93c8e3f3315b28f9109c96a
BoKyungKim <qhzud1207@gmail.com>
2026-04-27 14:53:39 +0900

    자동빌드 테스트02

--- backend/parking_backend/src/main/java/com/example/demo/global/config/kiosk/KioskSecurityConfig.java
+++ backend/parking_backend/src/main/java/com/example/demo/global/config/kiosk/KioskSecurityConfig.java
@@ -49,7 +49,7 @@ public class KioskSecurityConfig {
         // 허용할 오리진(리액트 주소 등) 설정
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5201",
-                "http://localhost:5202", //윤진추가 삭제예정
+                "http://localhost:5202", //윤진삭제예정
                 "http://localhost:5203",//윤진추가 삭제예정
                 "https://admin.parking-system.store",
                 "https://user.parking-system.store",
