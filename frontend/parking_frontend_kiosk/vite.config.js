fe92a07879aacf891f80d23afadcf7a205f5ee64
dbsekdgh <dbsekdgh09@gmail.com>
2026-04-29 17:10:17 +0900

    refactor: vite

--- frontend/parking_frontend_kiosk/vite.config.js
+++ frontend/parking_frontend_kiosk/vite.config.js
@@ -5,5 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envDir: '../../',
-  server: { port: 5203 },
+  server: {
+    port: 5203,
+    proxy: {
+      '/api': 'http://localhost:8081',
+    },
+  },
 })
