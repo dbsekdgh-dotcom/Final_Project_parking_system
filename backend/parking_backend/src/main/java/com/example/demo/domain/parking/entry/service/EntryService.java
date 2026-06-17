a4d4d3d9bc098d43d825fb6384540d7ae29428d6
dbsekdgh <dbsekdgh09@gmail.com>
2026-06-17 15:08:58 +0900

    fix:import optional 추가

--- backend/parking_backend/src/main/java/com/example/demo/domain/parking/entry/service/EntryService.java
+++ backend/parking_backend/src/main/java/com/example/demo/domain/parking/entry/service/EntryService.java
@@ -39,6 +39,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 
 @Service
